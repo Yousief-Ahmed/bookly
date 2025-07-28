ddcf0793d2b43f6cb9e3bbf7acfd2faa52568f5f
Yousief Ahmed <109359820+Yousief-Ahmed@users.noreply.github.com>
2025-07-28 21:48:29 +0300

    -Created the project - created splash screen with animation

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+
+const primaryColor = Color(0xff100B20);
+const transitionDuration = Duration(milliseconds: 250);
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/features/splash/presentaion/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
+        home: SplashView(),
+      ),
+    );
+  }
+}
